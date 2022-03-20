cd1c68534bcda1440e556071a4f1e7864253f0c9
sinfonian01 <sinfonian01@gmail.com>
2022-03-19 20:09:39 -0500

    Adding has password and user object validation

--- model/User.js
+++ model/User.js
@@ -1,27 +1,38 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    username:{
+    name:{
         type: String,
-        require: true,
-        min: 6,
+        require: [true, "Name is required"],
+        min: 2,
         max: 255
     },
     password:{
         type: String,
-        require: true,
+        require: [true, "Password is required"],
         min: 8,
         max: 1024
     },
     email:{
         type: String,
-        require: true,
-        min: 6
+        require: [true, "Email is required"],
+        min: 6,
+        trim: true
+        ,validate: {
+            validator: function(v){
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+            },
+            message: "Please enter a valid email"
+        }
     },
     dt:{
         type: Date,
         default: Date.now
     }
 });
+
+userSchema.path('password').validate(function(v){
+    return v && v.length >= 8;
+}, "Password must be at least 8 characters in length");
 
 module.exports = mongoose.model('User', userSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,32 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 const User = require('../model/User');
 
 router.post('/register', async (req, res) => {
+    const body  = req.body;
+    //create the new user object
     const user = new User({
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email
+        name: body.username,
+        password: body.password,
+        email: body.email
     });
 
     try{
+        //Validate user object
+        const validationErrors = user.validateSync();
+        if(validationErrors) return res.status(400).send(validationErrors.message).json();
+        //Does user already exist?
+        const userExists = await User.findOne({email: user.email});
+        if(userExists) return res.status(400).send('User already exists').json();
+        //Geneate salt and hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(body.password, salt);
+        user.password = hashedPassword;
+        //Register User to database
         const savedUser = await user.save();
         res.send(savedUser);
     } catch(err){
-        res.status(500).send(err);
+        res.status(400).send(err).json();
     }
 });
 
